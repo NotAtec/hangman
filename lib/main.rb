6413fa20cbd340a536d35fdf5fe9f646ea12b789
Jeroen B <jeroenb04@live.com>
2022-01-12 18:37:09 +0100

    Remove PRY requirement

--- lib/main.rb
+++ lib/main.rb
@@ -4,7 +4,6 @@ require_relative 'round_management'
 require_relative 'player_input'
 require_relative 'round'
 require_relative 'game'
-require 'pry-byebug'
 
 def load?
   puts 'Do you want to load a previously saved round? (y/n)'
