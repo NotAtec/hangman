ba2f8b89b680cf6cc9efa6e048779c63859a89bf
Jeroen B <jeroenb04@live.com>
2022-01-12 17:22:41 +0100

    Added method to save the gameplay

--- lib/round_management.rb
+++ lib/round_management.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'date'
 # Module with methods to manage loading & creating rounds
 module RoundManagement
   def load_round; end
@@ -8,5 +9,22 @@ module RoundManagement
     @round_instance = Round.new
   end
 
-  def save_round; end
+  def save_round
+    File.open("saves/#{random_string}.yml", 'w') { |f| f.puts YAML.dump(@round_instance) }
+  end
+
+  def random_string
+    random = []
+    3.times { random << grab_word }
+    random.join('')
+  end
+
+  def grab_word
+    loop do
+      sample = File.readlines('5desk.txt').sample.chomp
+      next if sample.length < 5 || sample.length > 12
+
+      return sample
+    end
+  end
 end
